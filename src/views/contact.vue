f3216df2387ab721ac1917cce7b82ba28f27a063
MMikuto <jirawat4956@gmail.com>
2026-03-11 01:51:40 -0700

    Initial commit

---
+++ src/views/contact.vue
@@ -0,0 +1,24 @@
+<template lang="">
+    <div class="contaner">
+        <div class="alert alert-primary" role="alert">
+         A simple primary alert—check it out!
+        </div>
+
+        <h1>Show Contact</h1>
+        <p>jirawat chaithawornkit</p>
+    </div>
+
+
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style >
+h3{
+    color: #ff00f2;
+}
+    
+
+</style>
